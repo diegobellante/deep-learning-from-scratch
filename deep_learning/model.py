a91fd760429959be4fb9eccfcd0f84a468c0728a
diegobellante <diegobellante@gmail.com>
2026-04-19 20:45:07 -0300

    Starting Lenet-5 implementation

--- deep_learning/model.py
+++ deep_learning/model.py
@@ -56,6 +56,78 @@ class Softmax:
         # Gradient fused with CrossEntropy loss.
         # Only valid when paired with CrossEntropy — do not use with other loss functions.
         return 1
+
+class Flatten:
+    def __init__(self):
+        print(f'Flatten layer')
+        
+    def forward(self, layer_inputs):  
+        ret = layer_inputs.reshape(layer_inputs.shape[0],-1)
+        return ret
+        
+    def backward(self, da):
+        return da
+        
+class AVGPooling:
+
+    def __init__(self,kernel_size):
+        self.kernel_size=kernel_size
+        print(f'AVGPooling layer with kernel size: {self.kernel_size[0]} x {self.kernel_size[1]}')
+
+    def forward(self, layer_inputs):     #layer_inputs shape=(n, 6, 28, 28))  
+        n_batch=layer_inputs.shape[0]
+        self.pool = np.zeros((n_batch, layer_inputs.shape[1],int(layer_inputs.shape[2]/ self.kernel_size[0]),int(layer_inputs.shape[3]/ self.kernel_size[1])))
+        row=0
+        col=0
+
+        for c in range(self.pool.shape[1]):
+            for row in range(self.pool.shape[2]): 
+                for col in range(self.pool.shape[3]): 
+                    input_slice=layer_inputs[:,c,row*self.kernel_size[0]:row*self.kernel_size[0]+self.kernel_size[0], col*self.kernel_size[1]: col*self.kernel_size[1]+self.kernel_size[1]]
+                    self.pool[:,c,row,col]=np.mean(input_slice)
+        return self.pool
+
+    def backward(self, da):  
+        return da
+      
+class Convolution2D: # filter/kernel slides in 2 dimensions (Height and Width).
+
+    def __init__(self,  kernel_size, n_kernels, stride, input_shape, padding, activation_function, lr=0.1, seed=None):
+        rng = np.random.default_rng(seed)
+        self.lr = lr
+        self.input_shape = input_shape
+        self.n_kernels = n_kernels
+        self.kernel_size = kernel_size
+        self.kernels =  rng.standard_normal((n_kernels,input_shape[0], kernel_size[0], kernel_size[1])) * 0.01  
+        self.stride = stride
+        self.padding = padding
+        self.activation_function = activation_function
+        self.b = np.zeros(n_kernels)          # (n_kernels,) one bias per filter/kernel
+        self.act_map_rows=int(((input_shape[1]-self.kernel_size[0]+2*self.padding)/self.stride)+1)
+        self.act_map_cols=int(((input_shape[2]-self.kernel_size[1]+2*self.padding)/self.stride)+1)
+        parameters=input_shape[0] * n_kernels* kernel_size[0]* kernel_size[1]+len(self.b);
+        neurons=n_kernels* self.act_map_rows* self.act_map_cols 
+        print(f'Convolutional 2D layer with: {parameters} trainable parameters, {neurons} neurons, {parameters*self.act_map_rows* self.act_map_cols} connections, Entrada: {input_shape}')
+        
+               
+    def forward(self, layer_inputs):#(N,1,28,28) (N,6, 28, 28) NCHW  (N,C,H,W) N=batch_size C=channel, H=height W=width
+        n_batch=layer_inputs.shape[0]
+        self.feature_map = np.zeros((n_batch, self.n_kernels, self.act_map_rows,self.act_map_cols)) # feature map zero-initialized
+        padded_inputs = np.pad(layer_inputs, pad_width=((0, 0),(0, 0), (self.padding, self.padding),(self.padding,self.padding)), mode='constant', constant_values=0) #padding only H and W .Shape (2, 1, 32, 32))
+        row=0
+        col=0
+    
+        for k in range(self.n_kernels):
+            for row in range(0,self.act_map_rows,self.stride):
+                for col in range(0,self.act_map_cols,self.stride):
+                    input_slice=padded_inputs[:,:,row:row+self.kernel_size[0], col: col+self.kernel_size[1]]
+                    self.feature_map[:,k,row,col]=(input_slice * self.kernels[k]).sum()+self.b[k]
+
+        self.activation_map=self.activation_function.forward(self.feature_map)
+        return self.activation_map
+
+    def backward(self, da):
+        return da
 class Dense:
 
     def __init__(self, inputs, neurons, activation_function, lr=0.1, seed=None):
@@ -66,6 +138,7 @@ class Dense:
         self.activation_function = activation_function
         self.xb = None
         self.a = None
+        print(f'Dense layer with: {neurons*inputs+neurons} trainable parameters, {neurons} neurons, {neurons*inputs+neurons} connections')
 
     def forward(self, layer_inputs):
         self.xb=layer_inputs # save inputs
@@ -75,6 +148,7 @@ class Dense:
 
     def backward(self, da):
         dz = da * self.activation_function.backward()   #(batch, n_neurons)
+        #print(f'{dz.T.shape} { self.xb.shape}')
         dW = dz.T @ self.xb   # weigth gradient  (neurons, inputs) — no dividtion by len(self.xb)  before:dW = dz.T @ self.xb / len(self.xb)   
         db = np.sum(dz, axis=0)   #bias gradient=   → shape (n_neurons,) sum instead mean  before: db = np.mean(dz, axis=0)
         da_prev = dz @ self.W             
@@ -85,7 +159,7 @@ class Dense:
         
    
         
-class MLP: # Multilayer Perceptron
+class SequentialNetwork: 
     def __init__(self, layers, loss_function):
         self.layers=layers
         self.loss_function = loss_function
@@ -106,7 +180,9 @@ class MLP: # Multilayer Perceptron
         for start in range(0, len(X), batch_size):
             xb = X[start:start+batch_size]  # x batch (batch, features)
             yb = y[start:start+batch_size]  # y batch (batch,)
+           
             y_pred = self.forward(xb) # (batch,last_layer_n_neurons)
+            
             loss = self.loss_function.forward(yb, y_pred)
             epoch_loss += loss
             # Backward
